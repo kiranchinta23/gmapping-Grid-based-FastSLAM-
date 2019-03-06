577dc15b1341fd51eca50666fe4c5f3127ed2a16
kiranchinta23 <kiran.chinta23@gmail.com>
2019-03-05 16:48:16 -0800

    initial commit

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/kiran/gmapping/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/kiran/gmapping/devel;/opt/ros/kinetic".split(';') if "/home/kiran/gmapping/devel;/opt/ros/kinetic" != "" else []
